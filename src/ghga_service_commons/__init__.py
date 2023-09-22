9e011471cff3f94b5b5bafca90b2b5661d8fa720
TheByronHimes <TheByronHimes@gmail.com>
2023-09-22 15:17:09 +0000

    Transition to pip-tools/pyproject.toml

--- src/ghga_service_commons/__init__.py
+++ src/ghga_service_commons/__init__.py
@@ -15,4 +15,6 @@
 
 """A library that contains the common functionality used in services of GHGA"""
 
-__version__ = "0.7.0"
+from importlib.metadata import version
+
+__version__ = version(__package__)
