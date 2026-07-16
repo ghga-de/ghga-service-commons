a08f2e87f00840066951e49a624c0a28b13e37a3
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2026-07-16 12:22:53 +0000

    Another revert

--- examples/auth_demo/auth_demo/inject.py
+++ examples/auth_demo/auth_demo/inject.py
@@ -36,7 +36,7 @@ from ghga_service_commons.utils.context import asyncnullcontext
 def prepare_core(
     *,
     config: Config,
-) -> Generator[HangoutPort]:
+) -> Generator[HangoutPort, None, None]:
     """Constructs and initializes all core components and their outbound dependencies."""
     yield Hangout(config=config)
 
