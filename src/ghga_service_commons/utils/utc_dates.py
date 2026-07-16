382aade17c1a6b923949191490ae9d8a501e6557
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2026-07-16 12:16:25 +0000

    Revert timezone code

--- src/ghga_service_commons/utils/utc_dates.py
+++ src/ghga_service_commons/utils/utc_dates.py
@@ -17,7 +17,7 @@
 
 from __future__ import annotations
 
-from datetime import UTC, datetime
+from datetime import datetime, timezone
 from typing import Annotated
 
 from pydantic import AwareDatetime, TypeAdapter
@@ -25,7 +25,7 @@ from pydantic.functional_validators import BeforeValidator
 
 __all__ = ["UTC", "UTCDatetime", "assert_tz_is_utc", "convert_tz_to_utc", "now_as_utc"]
 
-UTC = UTC
+UTC = timezone.utc
 
 
 def assert_tz_is_utc() -> None:
